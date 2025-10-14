c8822de3d869cef651c4d6e23624d5ae3ae8ca82
monokaijs <monokaijs@gmail.com>
2025-10-15 04:39:20 +0700

    fix: update source id

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,63 @@
-import { MusicSource } from "./types/source";
+import * as node_modules_youtubei_js_dist_src_parser_ytmusic from 'node_modules/youtubei.js/dist/src/parser/ytmusic';
+import * as node_modules_youtubei_js_dist_src_parser_helpers from 'node_modules/youtubei.js/dist/src/parser/helpers';
+import * as node_modules_youtubei_js_dist_src_parser_nodes from 'node_modules/youtubei.js/dist/src/parser/nodes';
+
+type TrackSource = 'youtube' | 'youtube-music' | 'file';
+interface Track {
+    id: string;
+    title: string;
+    artist: string;
+    artwork: string;
+    duration: number;
+    url?: string;
+    videoId: string;
+    isOffline?: boolean;
+    localPath?: string;
+    addedAt: number;
+    isLoading?: boolean;
+    source?: TrackSource;
+}
+interface VideoSearchResult {
+    id: string;
+    title: string;
+    artist: string;
+    thumbnail: string;
+    duration: number;
+    viewCount?: string;
+}
+interface SearchResponse {
+    results: VideoSearchResult[];
+    nextPageToken?: string;
+}
+
+interface MusicSource {
+    id: string;
+    name: string;
+    version: string;
+    initialize(): Promise<void>;
+    search(query: string, pageToken?: string, type?: 'web' | 'music'): Promise<SearchResponse>;
+    getPlayableUrl(trackId: string, source?: string): Promise<string>;
+    getSuggestions(trackId: string, type?: 'web' | 'music', size?: number): Promise<Track[]>;
+    getPlaylists?(): Promise<any[]>;
+    getPlaylist?(playlistId: string): Promise<any>;
+    getSearchSuggestions?(query: string): Promise<string[]>;
+    getTopTracks?(): Promise<any>;
+}
+
 declare class YouTubeSource implements MusicSource {
     id: string;
     name: string;
     version: string;
     initialize(): Promise<void>;
-    search(query: string, pageToken: string | undefined, type?: 'web' | 'music'): Promise<import("./types/music").SearchResponse>;
+    search(query: string, pageToken: string | undefined, type?: 'web' | 'music'): Promise<SearchResponse>;
     getPlayableUrl(trackId: string): Promise<string>;
-    getSuggestions(trackId: string, type?: 'web' | 'music', size?: number): Promise<import("./types/music").Track[]>;
-    getPlaylists(): Promise<import("node_modules/youtubei.js/dist/src/parser/helpers").ObservedArray<import("node_modules/youtubei.js/dist/src/parser/nodes").GridPlaylist | import("node_modules/youtubei.js/dist/src/parser/nodes").LockupView | import("node_modules/youtubei.js/dist/src/parser/nodes").Playlist>>;
-    getPlaylist(playlistId: string): Promise<import("node_modules/youtubei.js/dist/src/parser/ytmusic").Playlist>;
+    getSuggestions(trackId: string, type?: 'web' | 'music', size?: number): Promise<Track[]>;
+    getPlaylists(): Promise<node_modules_youtubei_js_dist_src_parser_helpers.ObservedArray<node_modules_youtubei_js_dist_src_parser_nodes.GridPlaylist | node_modules_youtubei_js_dist_src_parser_nodes.LockupView | node_modules_youtubei_js_dist_src_parser_nodes.Playlist>>;
+    getPlaylist(playlistId: string): Promise<node_modules_youtubei_js_dist_src_parser_ytmusic.Playlist>;
     getSearchSuggestions(query: string): Promise<string[]>;
 }
 declare const defaultSource: YouTubeSource;
-export default defaultSource;
-export declare const createSource: () => YouTubeSource;
+
+declare const createSource: () => YouTubeSource;
+
+export { createSource, defaultSource as default };
